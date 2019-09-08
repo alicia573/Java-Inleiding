ed9e79d7f160de089a955c4ed6604af1d722c4a0
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-09-08 17:53:51 +0200

    commit

---
+++ src/h05/berekening3.java
@@ -0,0 +1,34 @@
+package h05;
+
+
+import java.awt.*;
+import java.applet.*;
+
+public class berekening3 extends Applet {
+    int a;
+    int b;
+
+
+
+    int uitkomst;
+
+    public void init () {
+
+        a= 9;
+        b= 16;
+
+        uitkomst= (9-16);
+    }
+    public void paint(Graphics g) {
+        g.drawString("Antwoord= "+uitkomst, 50, 50);
+
+
+
+    }
+
+
+
+
+
+
+}
