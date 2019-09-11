64e2da97b2b3600bf33e73a4bca1ddbe74280b2d
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-09-11 11:42:43 +0200

    commit

---
+++ src/h10/praktijkopdracht.java
@@ -0,0 +1,83 @@
+package h10;
+
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+
+public class praktijkopdracht extends Applet {
+    TextField tekstvak;
+    Label label;
+    String s, tekst;
+    int Cijfers;
+
+    public void init() {
+        tekstvak = new TextField("", 20);
+        label = new Label("Type een cijfer in : ");
+        tekstvak.addActionListener( new TekstvakListener() );
+        tekst = "";
+
+        add(label);
+        add(tekstvak);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(tekst, 50, 60 );
+
+    }
+
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+            Cijfers = Integer.parseInt( s);
+            switch(Cijfers) {
+                case 1:
+                    tekst = "Slecht";
+
+                    break;
+                case 2:
+                    tekst = "Slecht";
+
+                    break;
+                case 3:
+                    tekst = "Slecht";
+
+                    break;
+                case 4:
+                    tekst = "Onvoldoene";
+
+                    break;
+                case 5:
+                    tekst = "Matig";
+
+                    break;
+                case 6:
+                    tekst = "Voldoende";
+
+                    break;
+                case 7:
+                    tekst = "Voldoende";
+
+                    break;
+                case 8:
+                    tekst = "Goed";
+
+                    break;
+                case 9:
+                    tekst = "Goed";
+
+                    break;
+                case 10:
+                    tekst = "Goed";
+
+                    break;
+                default:
+                    tekst = "Verkeerd cijfer!";
+                    break;
+            }
+            repaint();
+        }
+    }
+}
+
