dd6222cdaebbeb3fc701aa6f1e9e67abb31e5f37
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-09-11 12:08:39 +0200

    commit

---
+++ src/h11/lijnen.java
@@ -0,0 +1,59 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class lijnen  extends Applet{
+
+
+
+
+
+
+
+
+
+    public void paint(Graphics g) {
+        g.drawRect (200, 80, 1, 400);
+        g.drawRect (220, 80, 1, 400);
+        g.drawRect (240, 80, 1, 400);
+        g.drawRect (260, 80, 1, 400);
+        g.drawRect (280, 80, 1, 400);
+        g.drawRect (300, 80, 1, 400);
+        g.drawRect (320, 80, 1, 400);
+        g.drawRect (340, 80, 1, 400);
+        g.drawRect (360, 80, 1, 400);
+        g.drawRect (380, 80, 1, 400);
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
