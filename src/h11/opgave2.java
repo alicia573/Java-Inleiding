420cc34e3e0bb1b8d0811a65f7818ef9ccaf531c
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-09-12 13:51:03 +0200

    commit

---
+++ src/h11/opgave2.java
@@ -0,0 +1,107 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class opgave2 extends Applet {
+    Font f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11;
+
+
+    public void init() {
+        setBackground(Color.white);
+        f1= new Font("Arial",Font.PLAIN,45);
+        f2= new Font("Arial",Font.PLAIN,45);
+        f3= new Font("Arial",Font.PLAIN,45);
+        f4= new Font("Arial",Font.PLAIN,45);
+        f5= new Font("Arial",Font.PLAIN,45);
+        f6= new Font("Arial",Font.PLAIN,45);
+        f7= new Font("Arial",Font.PLAIN,45);
+        f8= new Font("Arial",Font.PLAIN,45);
+        f9= new Font("Arial",Font.PLAIN,45);
+        f10= new Font("Arial",Font.PLAIN,45);
+        f11= new Font("Arial",Font.PLAIN,45);
+    }
+
+
+
+    public void paint (Graphics g){
+        g.setFont (f1);
+        g.drawString (String.valueOf (10), 300, 500);
+        g.setFont (f2);
+        g.drawString (String.valueOf (11), 360, 500);
+        g.setFont (f3);
+        g.drawString (String.valueOf (12), 420,500);
+        g.setFont (f4);
+        g.drawString (String.valueOf (13), 480, 500);
+        g.setFont (f5);
+        g.drawString (String.valueOf (14), 540, 500);
+        g.setFont (f6);
+        g.drawString (String.valueOf (15), 600, 500);
+        g.setFont (f7);
+        g.drawString (String.valueOf (16), 660, 500);
+        g.setFont (f8);
+        g.drawString (String.valueOf (17), 720, 500);
+        g.setFont (f9);
+        g.drawString (String.valueOf (18), 780, 500);
+        g.setFont (f10);
+        g.drawString (String.valueOf (19), 840, 500);
+        g.setFont (f11);
+        g.drawString (String.valueOf (20), 900, 500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
