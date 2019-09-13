0147442bbdae814e845755210fd27aae4c93dfb4
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-09-13 11:39:56 +0200

    commit

---
+++ src/h11/Opgave6.java
@@ -0,0 +1,34 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opgave6 extends Applet {
+
+
+
+    public void init(){
+
+
+
+
+
+
+    }
+
+    public void paint ( Graphics g){
+        g.drawOval (900,500, 50, 50);
+        g.drawOval (890,490, 70, 70);
+        g.drawOval (880,480, 90, 90);
+        g.drawOval (870,470, 110, 110);
+        g.drawOval (860,460,130 , 130);
+
+
+
+
+    }
+
+
+
+
+}
