0d6428816d834a6cc49c7ffed13e894110fb4412
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-09-18 09:52:57 +0200

    commit

--- src/h12/Opgave2.java
+++ src/h12/Opgave2.java
@@ -5,64 +5,20 @@ import java.awt.*;
 
 
 public class Opgave2 extends Applet {
-   Button knopa;
+    int[] numbers = new int[5];
+    Button[] buttons = new Button[25];
 
     public void init() {
         setSize (300, 400);
-        knopa= new Button("1");
-        add(knopa);
-        knopa= new Button("2");
-        add(knopa);
-        knopa= new Button("3");
-        add(knopa);
-        knopa= new Button("3");
-        add(knopa);
-        knopa= new Button("4");
-        add(knopa);
-        knopa= new Button("5");
-        add(knopa);
-        knopa= new Button("6");
-        add(knopa);
-        knopa= new Button("7");
-        add(knopa);
-        knopa= new Button("8");
-        add(knopa);
-        knopa= new Button("9");
-        add(knopa);
-        knopa= new Button("10");
-        add(knopa);
-        knopa= new Button("11");
-        add(knopa);
-        knopa= new Button("12");
-        add(knopa);
-        knopa= new Button("13");
-        add(knopa);
-        knopa= new Button("14");
-        add(knopa);
-        knopa= new Button("15");
-        add(knopa);
-        knopa= new Button("16");
-        add(knopa);
-        knopa= new Button("17");
-        add(knopa);
-        knopa= new Button("18");
-        add(knopa);
-        knopa= new Button("19");
-        add(knopa);
-        knopa= new Button("20");
-        add(knopa);
-        knopa= new Button("21");
-        add(knopa);
-        knopa= new Button("22");
-        add(knopa);
-        knopa= new Button("23");
-        add(knopa);
-        knopa= new Button("24");
-        add(knopa);
-        knopa= new Button("25");
-        add(knopa);
+        add (buttons);
+        for (int Button = 0; Button < 25; Button++) {
+            buttons[Button] = new Button ("Button");
+            add (buttons[Button]);
 
 
-
+        }
     }
+
+    private void add(Button[] buttons) {
     }
+}
