334f270ba211f096e5453fbcfb52d677ffb62562
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-10-15 08:48:55 +0200

    commit

--- src/h13/Praktijkopdracht1.java
+++ src/h13/Praktijkopdracht1.java
@@ -6,21 +6,34 @@ import java.awt.*;
 public class Praktijkopdracht1 extends Applet {
 
     public void init(){
+
         setSize(500,300);
 
+
     }
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
+        int y1=100, y2=70;
+        for (int i = 0; i < 20; i++) {
+            bomen (g,  y1,  y2 );
+            y1++;
+            y2++;
+
+        }
+
+        bomen  (g, y1, y2);
+    }
+        void bomen ( Graphics g,  int y1,int y2){
         g.setColor(new Color(127, 51, 0));
-        g.fillRect (100,100,20,100);
-        g.fillRect (200,100,20,100);
-        g.fillRect (300,100,20,100);
-        g.fillRect (400,100,20,100);
+        g.fillRect (100,y1,20,100);
+        g.fillRect (200,y1,20,100);
+        g.fillRect (300,y1,20,100);
+        g.fillRect (400,y1,20,100);
 
         g.setColor(Color.GREEN);
-        g.fillOval (75, 70 ,70, 70);
-        g.fillOval (175, 70 ,70, 70);
-        g.fillOval (275, 70 ,70, 70);
-        g.fillOval (375, 70 ,70, 70);
+        g.fillOval (75, y2 ,70, 70);
+        g.fillOval (175, y2 ,70, 70);
+        g.fillOval (275, y2 ,70, 70);
+        g.fillOval (375, y2 ,70, 70);
 
     }
 }
