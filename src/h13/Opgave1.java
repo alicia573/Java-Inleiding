697413e96c4e1f2c8966e4d98194e2c5927f22b0
alicia573 <54794745+alicia573@users.noreply.github.com>
2019-10-15 09:53:33 +0200

    commit

--- src/h13/Opgave1.java
+++ src/h13/Opgave1.java
@@ -10,7 +10,7 @@ public class Opgave1 extends Applet {
         int x1, y1, x2, y2, x3,y3;
         x1 = x2 = x3 = 160;
         y1 = y2 = y3 = 120;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 300; i++) {
             driehoek (g, x1, y1, x2, y2, x3, y3);
             x3++;
             x2--;
--- src/h13/Praktijkopdracht2.java
+++ src/h13/Praktijkopdracht2.java
@@ -10,30 +10,39 @@ public class Praktijkopdracht2 extends Applet {
 
     }
     public void paint(Graphics g){
-        g.setColor(new Color(127, 51, 0));
-        g.fillRect (100,100,20,100);
-        g.fillRect (200,100,20,100);
-        g.fillRect (300,100,20,100);
-        g.fillRect (400,100,20,100);
-        g.fillRect (500,100,20,100);
+        int y1=100, width= 70, height=70 ;
+        for (int i = 0; i < 20; i++){
+            bomen (g, y1, width, height );
 
-        g.fillRect (100,350,20,100);
-        g.fillRect (200,350,20,100);
-        g.fillRect (300,350,20,100);
-        g.fillRect (400,350,20,100);
-        g.fillRect (500,350,20,100);
+        }
+        bomen (g, y1, width,height);
+
+    }
+        void bomen (Graphics g, int y1, int height, int width){
+        g.setColor(new Color(127, 51, 0));
+        g.fillRect (y1,y1,20,y1);
+        g.fillRect (200,y1,20,y1);
+        g.fillRect (300,y1,20,y1);
+        g.fillRect (400,y1,20,y1);
+        g.fillRect (500,y1,20,y1);
+
+        g.fillRect (y1,350,20,y1);
+        g.fillRect (200,350,20,y1);
+        g.fillRect (300,350,20,y1);
+        g.fillRect (400,350,20,y1);
+        g.fillRect (500,350,20,y1);
 
         g.setColor(Color.GREEN);
-        g.fillOval (75, 70 ,70, 70);
-        g.fillOval (175, 70 ,70, 70);
-        g.fillOval (275, 70 ,70, 70);
-        g.fillOval (375, 70 ,70, 70);
-        g.fillOval (475, 70 ,70, 70);
+        g.fillOval (75, 70 ,width, height);
+        g.fillOval (175, 70 ,width, height);
+        g.fillOval (275, 70 ,width, height);
+        g.fillOval (375, 70 ,width, height);
+        g.fillOval (475, 70 ,width, height);
 
-        g.fillOval (75, 300 ,70, 70);
-        g.fillOval (175, 300 ,70, 70);
-        g.fillOval (275, 300 ,70, 70);
-        g.fillOval (375, 300 ,70, 70);
-        g.fillOval (475, 300 ,70, 70);
+        g.fillOval (75, 300 ,width, height);
+        g.fillOval (175, 300 ,width, height);
+        g.fillOval (275, 300 ,width, height);
+        g.fillOval (375, 300 ,width, height);
+        g.fillOval (475, 300 ,width, height);
     }
 }
